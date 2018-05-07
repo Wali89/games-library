b43abc1560ae3631c7bd6a670c7fa2139320dfd4
wali89 <wali.rahman89@yahoo.com>
2018-05-07 06:55:36 -0400

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,5 +24,30 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/login' do 
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect '/games'
+    end
+  end
 
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/games"
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 end
