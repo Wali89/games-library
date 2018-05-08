483153a16c476b6a8c7929e309eada9e0d7ce34f
wali89 <wali.rahman89@yahoo.com>
2018-05-08 07:22:53 -0400

    Fix games

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ActiveRecord::Base
   belongs_to :platform
 
   def self.valid_params?(params)
-    return !params[:name].empty? && !params[:manufacturer].empty
+    return !params[:name].empty? && !params[:players].empty
   end
 end
