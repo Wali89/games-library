dec70cd82a30ec818334a09812f134b36c4575e5
wali89 <wali.rahman89@yahoo.com>
2018-05-08 07:16:54 -0400

    Fix platforms

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,11 +1,6 @@
 class Platform < ActiveRecord::Base
-  has_many :game_platforms
-  has_many :games, :through => :game_platforms
-  has_many :users, :through => :games
-  
-  def slug
-    name.downcase.gsub(" ","-")
-  end
+  has_many :games
+  belongs_to :user
   
   def self.valid_params?(params)
     return !params[:name].empty? && !params[:capacity].empty?
