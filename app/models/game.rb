8b3b6a1d37eb2ddd3a66d144b1ca1e5fe6b8dd59
wali89 <wali.rahman89@yahoo.com>
2018-05-14 15:46:50 -0400

    Made changes so that user muust input an integer above 0 for the number of players

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ActiveRecord::Base
   belongs_to :device
   
   def self.valid_params?(params)
-    return !params[:name].empty? && !params[:players].empty?
+    return !params[:name].empty? && !params[:players].empty? && params[:players].to_i > 0
   end
 end
